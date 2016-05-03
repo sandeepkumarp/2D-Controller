51ce05819a3bff874b1af468db9491569267f574
Sandeep <sandeep.pilakhnawal@gmail.com>
2016-05-03 22:32:33 +0530

    Basic Setup and Rayasting

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    const float skinWidth = 0.015f;
+    public int horizontalRayCount = 4;
+    public int VerticalRayCount = 4;
+
+    float horizontalRaySpacing, verticalRaySpacing;
+
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight,
+            bottomLeft, bottomRight;
+    }
+
+    void updateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.x);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        VerticalRayCount = Mathf.Clamp(VerticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (VerticalRayCount - 1);
+    }
+
+    void Update()
+    {
+        updateRaycastOrigins();
+        CalculateRaySpacing();
+
+        for (int i = 0; i < VerticalRayCount; i++)
+        {
+            Debug.DrawRay(raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+
+    }
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+
+}
